1581bf5374fe701fb87e7e9e09669601b4a15ca5
ianknowles <ian@imknowles.co.uk>
2020-10-27 18:23:18 +0000

    update authorship info
    Signed-off-by: ianknowles <ian@imknowles.co.uk>

--- server/app/models/Acknowledgements.scala
+++ server/app/models/Acknowledgements.scala
@@ -5,7 +5,13 @@ import controllers.routes.Assets
 object Acknowledgements {
 	val primary: List[Acknowledgement] = List(
 		Acknowledgement(
-			href = "https://www.ox.ac.uk/",
+			href = "https://www.thebritishacademy.ac.uk",
+			alt = "British Academy logo",
+			image = Assets.versioned("images/logos/BA_Primary-Logo-Black.png"),
+			imageCSSClass = "ba",
+		),
+		Acknowledgement(
+			href = "https://www.sociology.ox.ac.uk/",
 			alt = "University of Oxford logo",
 			image = Assets.versioned("images/logos/oxweb-logo-rect.svg"),
 			imageCSSClass = "oxford",
